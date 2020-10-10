93ea570ce0f3324aeed49ce9ad24e98bffc70c98
Albertus Angga <albertusangga.98@gmail.com>
2020-10-10 17:20:14 +0700

    Setup Google Auth on Android/iOS/Web

--- lib/src/app/screens/home_screen.dart
+++ lib/src/app/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:konsrr/src/auth/screens/login_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
@@ -7,21 +9,31 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: Text("Konsrr App")),
         body: Center(
-          child: StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance.collection('events').snapshots(),
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) {
-                return CircularProgressIndicator();
-              }
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              StreamBuilder<QuerySnapshot>(
+                stream:
+                    FirebaseFirestore.instance.collection('events').snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return CircularProgressIndicator();
+                  }
 
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  for (final doc in snapshot.data.docs)
-                    Text(doc.data().toString())
-                ],
-              );
-            },
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      for (final doc in snapshot.data.docs)
+                        Text(doc.data().toString())
+                    ],
+                  );
+                },
+              ),
+              RaisedButton(
+                child: Text('Go to Sign In page'),
+                onPressed: () => Get.to(LoginScreen()),
+              )
+            ],
           ),
         ));
   }
